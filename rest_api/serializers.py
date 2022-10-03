33da2c6eaa88cc679494fcdee16e9b667c4bff78
abduazizdev <1002374m@gmail.com>
2022-10-03 14:24:15 +0500

    all

---
+++ rest_api/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import field
+from pyexpat import model
+from .models import Krosovka
+from rest_framework import serializers
+
+
+class KrosovkaAPI(serializers.ModelSerializer):
+    class Meta:
+        model = Krosovka
+        fields = '__all__'
--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import home
+from .views import home, krosovkaMakeAPI
 
 app_name = "api"
 
 urlpatterns = [
     path('', home, name="home"),
+    path('krosovka-api/', krosovkaMakeAPI),
 ]
